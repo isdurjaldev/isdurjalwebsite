8f5e60b35d7fd70b91359ff9338b7dd73da67412
isdurjaldev <is.durjal.dev@gmail.com>
2022-10-29 21:49:26 -0500

    comentado el contacto trabajando en envio de mensaje

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,39 @@
-let modal = document.getElementById("MsgBox");
-let btn = document.getElementById("msg");
-let span = document.getElementsByClassName("close")[0];
+// let modal = document.getElementById("MsgBox");
+// let btn = document.getElementById("msg");
+// let span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function()
-{
-    modal.style.display = "block";
-}
+// btn.onclick = function()
+// {
+//     modal.style.display = "block";
+// }
 
-span.onclick = function()
-{
-    modal.style.display = "none";
-}
+// span.onclick = function()
+// {
+//     modal.style.display = "none";
+// }
 
-window.onclick = function(event)
+// window.onclick = function(event)
+// {
+//     if (event.target == modal)
+//     {
+//         modal.style.display = "none";
+//     }
+// }
+
+let sender = document.getElementById("Correo");
+let description = document.getElementById("Descripcion");
+function enviarMail()
 {
-    if (event.target == modal)
-    {
-        modal.style.display = "none";
-    }
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: sender,
+        Password: "We-wont-diegmail",
+        To: 'alberto.diaz.sw@gmail.com',
+        From: sender,
+        Subject: "Hola encontre tu Website",
+        Body: description,
+    })
+        .then(function(message){
+            alert("Mail enviado correctamente")
+        });
 }
